b1f123f73646fc35158a9d9c209fcea1d902b81b
raoul <raoulesd@gmail.com>
2026-01-21 13:47:31 +0100

    restructure, add readme and requirements.txt

---
+++ 1.0/run_folder.py
@@ -0,0 +1,38 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from db9_getScore import getScore
+import shutil
+
+path = "processed"
+isExist = os.path.exists(path)
+if not isExist:
+   os.makedirs(path)
+   print("Made dir: processed")
+
+path = "toscan"
+isExist = os.path.exists(path)
+if not isExist:
+   os.makedirs(path)
+   print("Made dir: toscan")
+
+path = "old/errored"
+isExist = os.path.exists(path)
+if not isExist:
+   os.makedirs(path)
+   print("Made dir: errored")
+
+path = "./toscan"
+onlyfiles = [f for f in listdir("./toscan") if isfile(join("./toscan", f))]
+
+csvFile = open("results.csv", "a")
+
+for f in onlyfiles:
+    totalFileName = join("./toscan",f)
+    csvLine = getScore(totalFileName)
+    csvFile.write(f"{csvLine}\n")
+    csvFile.flush()
+    shutil.move(totalFileName, join("./processed", f), copy_function=shutil.copy2)
+
+csvFile.close()
+
---
+++ README.txt
@@ -0,0 +1,7 @@
+Make sure Python is installed on the system by checking its version. Otherwise, download it [here](https://www.python.org/downloads/).
+
+> python --version
+
+Install the required dependencies using the package installer for Python.
+
+> pip install -r requirements.txt
