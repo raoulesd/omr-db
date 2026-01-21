51f61ac593b2833ceebfb14645532383d87f8567
raoul <raoulesd@gmail.com>
2026-01-21 14:12:44 +0100

    gitignore now works properly

---
+++ 2.0/test_grader.py
@@ -0,0 +1,28 @@
+# Code from this file is based on PyImageSearch.com:
+# https://pyimagesearch.com/2016/10/03/bubble-sheet-multiple-choice-scanner-and-test-grader-using-omr-python-and-opencv/
+
+# import the necessary packages
+from imutils.perspective import four_point_transform
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
+# define the answer key which maps the question number
+# to the correct answer
+ANSWER_KEY = {0: 1, 1: 4, 2: 0, 3: 3, 4: 1}
+
+
+# load the image, convert it to grayscale, blur it
+# slightly, then find edges
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 75, 200)
--- README.txt
+++ README.txt
@@ -5,3 +5,5 @@ Make sure Python is installed on the system by checking its version. Otherwise, 
 Install the required dependencies using the package installer for Python.
 
 > pip install -r requirements.txt
+
+Example forms saved in `.../2.0/test_forms/` are omitted due to privacy reasons and are available upon request from the repository owner.
